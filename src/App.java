43f31fe403d057c6827a107e1e6fd0f3f3e679d4
Arda Sarı <ardasari4859@gmail.com>
2024-03-31 03:03:58 +0300

    Adding new book window for add book button

--- src/App.java
+++ src/App.java
@@ -11,6 +11,127 @@ import javafx.stage.Stage;
 
 public class App extends Application {
     
+    
+    
+    private void btnOkDetect(){
+        
+        //display stage
+        Stage secondStage = new Stage();
+        
+        VBox root = new VBox();
+        HBox main = new HBox(20);
+        root.setPrefSize(400, 500);
+
+        
+        VBox leftBox = new VBox(8);
+        leftBox.setAlignment(Pos.CENTER_LEFT);
+
+        VBox rightBox = new VBox(8);
+        rightBox.setAlignment(Pos.CENTER_RIGHT);
+
+        //LEFT////////////////////////////////////////////////////////////////////////////
+
+        Label titleLabel = new Label("Title");
+        titleLabel.setFont(new Font(25));
+
+        TextField titleTextField = new TextField();
+        titleTextField.setPrefWidth(180);
+
+        Label authorLabel = new Label("Author/Authors");
+        authorLabel.setFont(new Font(25));
+
+        TextField auTextField = new TextField();
+        auTextField.setPrefWidth(180);
+
+        Label isbnLabel = new Label("ISBN");
+        isbnLabel.setFont(new Font(25));
+
+        TextField isbnTextField = new TextField();
+        isbnTextField.setPrefWidth(180);
+
+        Label dateLabel = new Label("Date");
+        dateLabel.setFont(new Font(25));
+
+        DatePicker datepPicker = new DatePicker();
+
+        Label coverLabel = new Label("Cover");
+        coverLabel.setFont(new Font(25));
+
+        //upload thing
+
+        Label ratingLabel = new Label("Rating");
+        ratingLabel.setFont(new Font(25));
+
+        TextField ratingTextField = new TextField();
+        ratingTextField.setPrefWidth(180);
+
+
+
+        //right/////////////////////////////////////////////////////
+
+        Label subtitle = new Label("SubTitle");
+        subtitle.setFont(new Font(25));
+
+        TextField subtitleTextField = new TextField();
+        subtitleTextField.setPrefWidth(180);
+
+        Label translatorLabel = new Label("Translator/Translators");
+        translatorLabel.setFont(new Font(25));
+
+        TextField translatorTextField = new TextField();
+        translatorTextField.setPrefWidth(180);
+
+        Label publisherLabel = new Label("Publisher");
+        publisherLabel.setFont(new Font(25));
+
+        TextField publisherField = new TextField();
+        publisherField.setPrefWidth(180);
+
+        Label editionLabel = new Label("Edition");
+        editionLabel.setFont(new Font(25));
+
+        TextField editionField = new TextField();
+        editionField.setPrefWidth(180);
+
+        Label languagLabel = new Label("Language");
+        languagLabel.setFont(new Font(25));
+
+        TextField languageField = new TextField();
+        languageField.setPrefWidth(180);
+
+        Label tagLabel = new Label("Tag/Tags");
+        tagLabel.setFont(new Font(25));
+
+        TextField tagTextField = new TextField();
+        tagTextField.setPrefWidth(180);
+
+    
+
+
+        VBox.setMargin(leftBox, new Insets(8));
+        VBox.setMargin(rightBox, new Insets(8));
+        VBox.setMargin(main, new Insets(8));
+    
+
+
+
+        leftBox.getChildren().addAll(titleLabel,titleTextField,authorLabel,auTextField,isbnLabel,isbnTextField,dateLabel,datepPicker,coverLabel,ratingLabel,ratingTextField);
+
+        rightBox.getChildren().addAll(subtitle,subtitleTextField,translatorLabel,translatorTextField,publisherLabel,publisherField,editionLabel,editionField,languagLabel,languageField,tagLabel,tagTextField);
+
+        main.getChildren().addAll(leftBox,rightBox);
+
+        root.getChildren().addAll(main);
+
+
+
+        Scene scene = new Scene(root);
+        secondStage.setScene(scene);
+        secondStage.setTitle("Add Book");
+        secondStage.show();
+    }
+    
+    
     @Override
     public void start(Stage primaryStage) {
         VBox root = new VBox();
@@ -53,7 +174,7 @@ public class App extends Application {
         
         //main pane
         HBox firstLine = new HBox();
-        firstLine.setAlignment(Pos.BOTTOM_LEFT);
+        firstLine.setAlignment(Pos.CENTER);
 
         HBox secondLine = new HBox(8);
         secondLine.setAlignment(Pos.CENTER);
@@ -81,6 +202,8 @@ public class App extends Application {
         Button addButton = new Button("Add Book");
         addButton.setFont(new Font(14));
         addButton.setPrefSize(100.0, 45.0);
+
+        addButton.setOnAction(e->btnOkDetect());
         
         VBox.setMargin(firstLine, new Insets(8));
         VBox.setMargin(secondLine, new Insets(8));
@@ -98,6 +221,9 @@ public class App extends Application {
         primaryStage.setScene(scene);
         primaryStage.setTitle("PagePal");
         primaryStage.show();
+
+
+        
     }
     
     public static void main(String[] args) {
