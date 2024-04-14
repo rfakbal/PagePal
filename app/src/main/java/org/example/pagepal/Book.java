c7a1b5c0b3a19ddded4a5f2b347ca7aff4ac4438
RusenAkbal <rusenfirat@gmail.com>
2024-04-14 16:50:36 +0300

    Gradle Added

---
+++ app/src/main/java/org/example/pagepal/Book.java
@@ -0,0 +1,109 @@
+package org.example.pagepal;
+import java.util.ArrayList;
+//import java.time.LocalDate;
+
+public class Book {
+    private String title;
+    private String subTitle;
+    private ArrayList<String> author;
+    private ArrayList<String> translator;
+    private ArrayList<String> tag;
+    private String isbn;
+    private String publisher;
+    //private LocalDate date;
+    private String edition;
+    private String language;
+    private String rating;
+    //private (JPG) cover will be added, do not forget to add set and get also;
+
+    public Book(
+        String title, 
+        String subTitle, 
+        ArrayList<String> author, 
+        ArrayList<String> translator,
+        ArrayList<String> tag,
+        String isbn,
+        String publisher,
+       // LocalDate date,
+        String edition,
+        String language,
+        String rating
+    ){
+        this.title = title;
+        this.subTitle = subTitle;
+        this.author = author;
+        this.tag = tag;
+        this.isbn = isbn;
+        this.publisher = publisher;
+       // this.date = date;
+        this.edition = edition;
+        this.language = language;
+        this.rating = rating;
+    }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public void setSubTitle(String subTitle) { this.subTitle = subTitle; }
+
+    public void setAuthor(ArrayList<String> author) { this.author = author; }
+
+    public void setTranslator(ArrayList<String> translator) { this.translator = translator; }
+
+    public void setTag(ArrayList<String> tag) { this.tag = tag; }
+
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+
+    public void setPublisher(String publisher) { this.publisher = publisher; }
+
+    //public void setDate(LocalDate date) { this.date = date; }
+
+    public void setEdition(String edition) { this.edition = edition; }
+
+    public void setLanguage(String language) { this.language = language; }
+
+    public void setRating(String rating) { this.rating = rating; }
+
+    public ArrayList<String> getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSubTitle() {
+        return subTitle;
+    }
+
+    public ArrayList<String> getTag() {
+        return tag;
+    }
+
+    public ArrayList<String> getTranslator() {
+        return translator;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+    
+    public String getPublisher() {
+        return publisher;
+    }
+
+    /*public LocalDate getDate() {
+        return date;
+    }*/
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+}
---
+++ app/src/main/java/org/example/pagepal/Library.java
@@ -0,0 +1,182 @@
+package org.example.pagepal;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+//import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Library {
+    private ArrayList<Book> libraryBooks = new ArrayList<>();
+    private ArrayList<Book> displayBooks = new ArrayList<>();
+    private String filePath;
+
+    // public void edit(Book book) { }
+    public ArrayList<Book> searchBook(String input, String type) {
+        displayBooks.clear(); // Önceki aramaların sonuçlarını temizle
+
+        if ("Title".equals(type)) {
+            for (Book book : libraryBooks) {
+                if (book.getTitle().equals(input)) {
+                    displayBooks.add(book);
+                }
+            }
+        } else if ("Subtitle".equals(type)) {
+            for (Book book : libraryBooks) {
+                if (book.getSubTitle().equals(input)) {
+                    displayBooks.add(book);
+                }
+            }
+        } else if ("Author".equals(type)) {
+            for (Book book : libraryBooks) {
+                for (String author : book.getAuthor()) {
+                    if (author.equals(input)) {
+                        displayBooks.add(book);
+                        break; // İç içe döngüden çık
+                    }
+                }
+            }
+        } else if ("Translator".equals(type)) {
+            for (Book book : libraryBooks) {
+                for (String translator : book.getTranslator()) {
+                    if (translator.equals(input)) {
+                        displayBooks.add(book);
+                        break; // İç içe döngüden çık
+                    }
+                }
+            }
+        } else if ("Tag".equals(type)) {
+            for (Book book : libraryBooks) {
+                for (String tag : book.getTag()) {
+                    if (tag.equals(input)) {
+                        displayBooks.add(book);
+                        break; // İç içe döngüden çık
+                    }
+                }
+            }
+        } else if ("ISBN".equals(type)) {
+            for (Book book : libraryBooks) {
+                if (book.getIsbn().equals(input)) {
+                    displayBooks.add(book);
+                    break;
+                }
+            }
+        } else if ("Publisher".equals(type)) {
+            for (Book book : libraryBooks) {
+                if (book.getPublisher().equals(input)) {
+                    displayBooks.add(book);
+                }
+            }
+        } /*else if ("Date".equals(type)) {
+            for (Book book : libraryBooks) {
+                if (book.getDate().equals(input)) {
+                    displayBooks.add(book);
+                }
+            }
+        }*/else if ("Edition".equals(type)) {
+            for (Book book : libraryBooks) {
+                if (book.getEdition().equals(input)) {
+                    displayBooks.add(book);
+                }
+            }
+        } else if ("Language".equals(type)) {
+            for (Book book : libraryBooks) {
+                if (book.getLanguage().equals(input)) {
+                    displayBooks.add(book);
+                }
+            }
+        } else if ("Rating".equals(type)) {
+            for (Book book : libraryBooks) {
+                if (book.getRating().equals(input)) {
+                    displayBooks.add(book);
+                }
+            }
+        } else {
+            System.out.println("Geçersiz arama türü: " + type);
+        }
+
+        return displayBooks;
+    }
+
+    public void addBook(Book book) {
+        libraryBooks.add(book);
+    }
+
+    public void removeBook(Book book) {
+        libraryBooks.remove(book);
+    }
+
+    public void importJSON(String path) throws Exception {
+        //empty json file error - will be solved
+        Gson gson = new Gson();
+
+        JsonReader reader = new JsonReader(new FileReader(path));
+        Book[] data = gson.fromJson(reader, Book[].class);
+        
+        for(int i = 0;i<data.length;i++){
+            addBook(data[i]);
+        }
+
+    }
+
+    public void exportJSON(String path) {
+        Gson gson = new Gson();
+        String[] booksArray = new String[libraryBooks.size()];  
+        for (int i = 0; i < libraryBooks.size(); i++) {
+            booksArray[i] = gson.toJson(libraryBooks.get(i));
+        }
+        try (FileWriter writer = new FileWriter(path)) {
+            writer.write("[\n");
+            for (int i = 0; i < libraryBooks.size(); i++) {
+                writer.write("{\n");
+                writer.write("  \"title\": \"" + libraryBooks.get(i).getTitle() + "\",\n");
+                writer.write("  \"subTitle\": \"" + libraryBooks.get(i).getSubTitle() + "\",\n");
+                writer.write("  \"author\": " + gson.toJson(libraryBooks.get(i).getAuthor()) + ",\n");
+                writer.write("  \"translator\": " + gson.toJson(libraryBooks.get(i).getTranslator()) + ",\n");
+                writer.write("  \"tag\": " + gson.toJson(libraryBooks.get(i).getTag()) + ",\n");
+                writer.write("  \"isbn\": \"" + libraryBooks.get(i).getIsbn() + "\",\n");
+                writer.write("  \"publisher\": \"" + libraryBooks.get(i).getPublisher() + "\",\n");
+                writer.write("  \"edition\": \"" + libraryBooks.get(i).getEdition() + "\",\n");
+                writer.write("  \"language\": \"" + libraryBooks.get(i).getLanguage() + "\",\n");
+                writer.write("  \"rating\": \"" + libraryBooks.get(i).getRating() + "\"\n");
+                if(i==libraryBooks.size()-1){
+                    writer.write("}\n");
+                }
+                else{
+                    writer.write("},\n");
+                }
+            }
+            writer.write("]");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Book> getLibraryBooks() {
+        return libraryBooks;
+    }
+
+    public void setLibraryBooks(ArrayList<Book> libraryBooks) {
+        this.libraryBooks = libraryBooks;
+    }
+
+    public ArrayList<Book> getDisplayBooks() {
+        return displayBooks;
+    }
+
+    public void setDisplayBooks(ArrayList<Book> displayBooks) {
+        this.displayBooks = displayBooks;
+    }
+
+    public String getFilePath(){
+        return filePath;
+    }
+
+    public void setFilePath(String filePath){
+        this.filePath = filePath;
+    }
+
+}
